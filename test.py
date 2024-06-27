0b116adad2b3208beb20e28257da96ce59483c6a
degrootdev <65593471+degrootdev@users.noreply.github.com>
2024-06-27 13:26:24 +0200

    Add files via upload
    added function to test path serever

--- test.py
+++ test.py
@@ -1,2 +1,5 @@
-class main():
-    print("tets")
+file_name = __file__.split('/')[-1]
+path = __file__.replace(file_name, '')
+in_folder = path + "test/dataset/Lschijf_met_autorisatiematrix"
+label_folder = path + "test/dataset/Lschijf_met_autorisatiematrix_gelabeld"
+print(in_folder)
