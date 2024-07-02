854958147fd198b1b81085e3d3e718afaecbd0ad
degrootdev <65593471+degrootdev@users.noreply.github.com>
2024-07-02 10:40:38 +0200

    Add files via upload
    changed path

--- gen_py.py
+++ gen_py.py
@@ -4,7 +4,7 @@ file_name = __file__.split('\\')[-1]
 path = __file__.replace(file_name, '')
 folder_name = "test.zip"
 path += folder_name
-output = '..\\'+ path
+output = path
 
 gdown.download(url, output)
 
